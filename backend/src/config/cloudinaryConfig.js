143e17d3ee78994bd88b84e808154a3f19232bb4
tusharmal711 <tusharmal711@gmail.com>
2026-06-06 21:25:55 +0530

    error fixed

--- backend/src/config/cloudinaryConfig.js
+++ backend/src/config/cloudinaryConfig.js
@@ -1,5 +1,6 @@
 // src/config/cloudinaryConfig.js
-import { v2 as cloudinary } from "cloudinary";
+import pkg from "cloudinary";
+const { v2: cloudinary } = pkg;
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 import dotenv from "dotenv/config";
